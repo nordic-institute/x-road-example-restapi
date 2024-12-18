ab0ddfc2b685d11b4cb1b0c65afe59b0f79b62ba
andresrosenthal <andres.rosenthal@nortal.com>
2024-12-18 13:09:37 +0200

    Add README, LICENSE & publish capability

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
+    `maven-publish`
 }
 
 group = "org.niis"
@@ -15,7 +18,29 @@ java {
 }
 
 repositories {
+    mavenLocal()
     mavenCentral()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifact(tasks.named<BootJar>("bootJar").get()) {
+                classifier = "boot"
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://artifactory.niis.org/xroad-maven-releases/")
+            val snapshotsRepoUrl = uri("https://artifactory.niis.org/xroad-maven-snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            // 'publish' task expects xroadMavenRepositoryUsername xroadMavenRepositoryPassword gradle project
+            // properties to be present
+            name = "xroadMavenRepository"
+            credentials(PasswordCredentials::class)
+        }
+    }
 }
 
 dependencies {
